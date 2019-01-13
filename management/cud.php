53d011c168d1b57d85cc09535fcc6550518bb7f7
rjeth <40770876+rjeth@users.noreply.github.com>
2019-01-13 15:19:38 +0800

    commit suicide

--- management/cud.php
+++ management/cud.php
@@ -6,6 +6,7 @@
     $surname = $_POST['surname'];
     $gender = $_POST['gender'];
     $lservice = $_POST['lengthservice'];
+
     $type = $_POST['usertypeselect'];
     $username = $_POST['username'];
     $password = $_POST['password'];
@@ -20,13 +21,14 @@
     $action = $_POST['action'];
     $id = $_POST['id'];
     if ($action == 'Add'){
-        mysqli_query($connect, "INSERT INTO userinfo( sname, fname, mname, extname, div_id, sec_id,  pos_id, gender , userimg )VALUES('$sname' , '$fname' , '$mname' , '$extname' , '$divsel' , '$secsel', '$possel', '$gender' ,'$new_name')");
-        mysqli_query($connect, "INSERT INTO useracc( username, password , type )VALUES('$username' , MD5('$password'), 2)");
+        mysqli_query($connect, "INSERT INTO tbluserinfo ( userinfoSURNAME , userinfoFIRSTNAME, userinfoMIDDLENAME, userinfoGENDER, userinfoSERVICELENGTH, userinfoOFFICE, userinfoDIVISION, userinfoSECTION,  userinfoPOSITION, userinfoUNIT)
+                                VALUES('$surname' , '$firstname' , '$middlename' , '$gender' , '$lservice' , '$office', '$division', '$section' ,'$position' ,'$unit')");
+        mysqli_query($connect, "INSERT INTO tbluseraccount( useraccountUSERNAME , useraccountPASSWORD , useraccountTYPE )VALUES('$username' , MD5('$password'), '$type')");
         echo 'Added Successfully';
     }
     if ($action == 'Edit') {
-        mysqli_query($connect, "UPDATE userinfo SET sname = '$sname', fname = '$fname' , mname = '$mname',  extname = '$extname', div_id = '$divsel', sec_id = '$secsel', pos_id = '$possel',  gender = '$gender' WHERE id = $id");
-        mysqli_query($connect, "UPDATE useracc SET username = '$username', password = MD5('$password') WHERE id = $id");
+        mysqli_query($connect, "UPDATE tbluserinfo SET sname = '$sname', fname = '$fname' , mname = '$mname',  extname = '$extname', div_id = '$divsel', sec_id = '$secsel', pos_id = '$possel',  gender = '$gender' WHERE id = $id");
+        mysqli_query($connect, "UPDATE tbluseraccount SET username = '$username', password = MD5('$password') WHERE id = $id");
         echo 'Update Successfully';
     }
     if ($action == 'Delete') {
